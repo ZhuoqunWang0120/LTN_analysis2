ca09fd3911ba2b61599f049b8c90b92fe5bd36f4
Zhuoqun Wang <40522043+ZhuoqunWang0120@users.noreply.github.com>
2022-11-10 16:08:38 -0500

    Update fit_COAT.R

--- src/simulation/covariance_estimation/fit_COAT.R
+++ src/simulation/covariance_estimation/fit_COAT.R
@@ -8,7 +8,7 @@ source(paste0(WORK_DIR,"/src/simulation/covariance_estimation/COAT.R"))
 cachedir=paste0(WORK_DIR,"/cache/covariance_estimation/dtm/")
 resultdir=paste0(cachedir,'coat/')
 system(paste0('mkdir -p ',resultdir))
-cnt=readRDS(paste0(cachedir,'sim',SEED,'.rds'))$cnt
+cnt=readRDS(paste0(cachedir,'sim_ntop100seed',SEED,'.rds'))$cnt
 cnt[cnt==0]=0.5
 coat_est=coat(cnt)$sigma
 saveRDS(coat_est,paste0(resultdir,'est',SEED,'.rds'))
@@ -19,8 +19,8 @@ system(paste0('mkdir -p ',resultdir))
 modelcovs=c('hub','block','sparse')
 for (modelCov in 1:3){
   modelcov=modelcovs[modelCov]
-  clrtrue=readRDS(paste0(cachedir,modelcov,'clrcov_',i,'.rds'))
-  cnt=readRDS(paste0(cachedir,modelcov,'cnt_',i,'.rds'))
+  clrtrue=readRDS(paste0(cachedir,modelcov,'clrcov_',i,'ntop100.rds'))
+  cnt=readRDS(paste0(cachedir,modelcov,'cnt_',i,'ntop100.rds'))
   cnt[cnt==0]=0.5
   coat_est=coat(cnt)$sigma
   saveRDS(coat_est,paste0(resultdir,modelcov,'_est',i,'.rds'))
